007381e593a1359ed66b3e9887d2a3d73472100e
nils <nils@fedger.io>
2018-05-22 13:43:57 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kerndisc',
+      version='0.0.1',
+      description='Automatic Kernel Discovery for Gaussian Processes.',
+      url='https://github.com/BracketJohn/imPhysGP',
+      author='BracketJohn',
+      packages=['kerndisc'],
+      zip_safe=False)
