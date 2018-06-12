2f1d61f0ce646dda249cc0de73b11257e3d21856
nils <nils@fedger.io>
2018-06-12 19:28:43 +0200

    continued with cp, cw

--- src/kerndisc/expansion/grammars/_kernels.py
+++ src/kerndisc/expansion/grammars/_kernels.py
@@ -1,9 +1,10 @@
 """Module that maintains all kernels that are available for kernel construction."""
+import copy
 from functools import reduce
-from typing import Dict
+from typing import Dict, Optional
 
 import gpflow
-from gpflow import Param
+from gpflow import Param, transforms
 from gpflow.kernels import (ArcCosine,
                             Combination,
                             Constant,
@@ -45,77 +46,90 @@ BASE_KERNELS: Dict[str, gpflow.kernels.Kernel] = {
 
 
 class ChangePoint(Combination):
-    """Changepoint Kernel that switches from one kernel to another at a certain offset."""
+    """Changepoint (CP) Kernel that switches from one kernel to another at a certain offset.
 
-    def __init__(self, kern_list, offset, variance):
+    This kernels output is defined as:
+    ```
+        CP(k1, k2, θ)(X, X') = σ(X, θ) * k1(X, X') * σ(X', θ) + σ(X, θ) * k2(X, X') * σ(X', θ)
+    ```
+    Where `k1`, `k2` are kernel functions, `σ` is the sigmoid smooth step function. This kernel is useful
+    for quick transitions between two kernel functions. `θ = (offset, variance)` are parameters of the sigmoid function
+    that dictate at what (time-)point the transition occurs (`offset`) and what variance is added. `θ` is learned
+    during training and applied to `X`, `X2` before `σ` is calculated.
+
+    """
+
+    def __init__(self, k1: gpflow.kernels.Kernel, k2: gpflow.kernels.Kernel, offset: float, variance: float=1.0):
         """Initialize changepoint at location `offset`, with variance `variance`."""
-        super(ChangePoint, self).__init__(kern_list)
-        self.offset = Param(np.array(offset).astype(np.float64))
-        self.variance = Param(np.array(variance).astype(np.float64))
+        super(ChangePoint, self).__init__([k1, k2])
 
-    def K(self, X, X2=None):
+        self.offset = Param(offset, transform=transforms.positive)
+        self.variance = Param(variance, transform=transforms.positive)
+
+    def K(self, X: np.ndarray, X2: Optional[np.ndarray]=None):  # noqa: N802, N803
         """Calculate covariance matrix."""
-        if X2 is None:
-            X2 = X
-        XXT = self.variance * tf.squeeze(tf.matmul(tf.expand_dims(X, axis=1), tf.expand_dims(X, axis=2))) + self.offset
-        XXT2 = self.variance * tf.squeeze(tf.matmul(tf.expand_dims(X2, axis=1), tf.expand_dims(X2, axis=2))) + self.offset
-        sigm = tf.sigmoid(XXT)
-        sigm2 = tf.sigmoid(XXT2)
-        sig1 = tf.matmul(tf.expand_dims(sigm, axis=1),
-                         tf.expand_dims(sigm2, axis=0))
-        sig2 = tf.matmul(tf.expand_dims((1. - sigm), axis=1),
-                         tf.expand_dims((1. - sigm2), axis=0))
-        a1 = reduce(tf.multiply,
-                    [sig1, self.kern_list[0].K(X, X2)])
-        a2 = reduce(tf.multiply,
-                    [sig2, self.kern_list[1].K(X, X2)])
-        return reduce(tf.add, [a1, a2])
+        sigmoid_at_X = tf.sigmoid(self.variance * tf.squeeze(tf.expand_dims(X, axis=1) @ tf.expand_dims(X, axis=2)) + self.offset)  # noqa: N806
 
-    def Kdiag(self, X):
+        if X2 is None:
+            sigmoid_at_X2 = sigmoid_at_X  # noqa: N806
+        else:
+            sigmoid_at_X2 = tf.sigmoid(self.variance * tf.squeeze(tf.expand_dims(X2, axis=1) @  tf.expand_dims(X2, axis=2)) + self.offset)  # noqa: N806
+
+        k1_multiplier = tf.expand_dims(sigmoid_at_X, axis=1) @ tf.expand_dims(sigmoid_at_X2, axis=0)
+        k2_multiplier = tf.expand_dims((1. - sigmoid_at_X), axis=1) @ tf.expand_dims((1. - sigmoid_at_X2), axis=0)
+
+        summand_1 = reduce(tf.multiply, [k1_multiplier, self.kern_list[0].K(X, X2)])
+        summand_2 = reduce(tf.multiply, [k2_multiplier, self.kern_list[1].K(X, X2)])
+
+        return reduce(tf.add, [summand_1, summand_2])
+
+    def Kdiag(self, X: np.ndarray):  # noqa: N802, N803
         """Calculate diagonal of covariance matrix only, convenience method."""
-        XXT = self.variance * tf.squeeze(tf.matmul(tf.expand_dims(X, axis=1), tf.expand_dims(X, axis=2))) + self.offset
-        sigm = tf.sigmoid(XXT)
-        a1 = reduce(tf.multiply, [sigm, self.kern_list[0].Kdiag(X),
-                                  sigm])
-        a2 = reduce(tf.multiply, [(1. - sigm), self.kern_list[1].Kdiag(X),
-                                  1. - sigm])
-        return reduce(tf.add, [a1, a2])
+        sigmoid_at_X = tf.sigmoid(self.variance * tf.squeeze(tf.expand_dims(X, axis=1) @ tf.expand_dims(X, axis=2)) + self.offset)  # noqa: N806
+
+        summand_1 = reduce(tf.multiply, [sigmoid_at_X, self.kern_list[0].Kdiag(X), sigmoid_at_X])
+        summand_2 = reduce(tf.multiply, [(1. - sigmoid_at_X), self.kern_list[1].Kdiag(X), 1. - sigmoid_at_X])
+
+        return reduce(tf.add, [summand_1, summand_2])
 
 
 class ChangeWindow(Combination):
-    """Changewindow kernel that applies a kernel only in a range from some startingpoint `t_s` to endpoint `t_e`."""
+    """Changewindow (CW) Kernel that switches from one kernel to another in a certain range.
 
-    def __init__(self, kern_list, kernel_start, kernel_end, variance):
-        """Initialize changewindow with some kernel which only applies in a certain window, then the other kernel applies again."""
-        super(ChangePoint, self).__init__(kern_list)
-        self.start = Param(np.array(kernel_start).astype(np.float64))
-        self.end = Param(np.array(kernel_end).astype(np.float64))
-        self.variance = Param(np.array(variance).astype(np.float64))
+    This kernels output is defined as:
+    ```
+        CW(k1, k2)(X, X') = CP(CP(k1, k2, θ_1), k1, θ_2)(X, X')
+    ```
+    Where `CP` is the changepoint function from above, `θ_1 = (offset_cp_1, variance_cp_1)` dictates
+    at what point the transition from `k1` to `k2` occurs, `θ_2 = (offset_cp_2, variance_cp_2)` then
+    forces the second transition from `k2` to `k1` at point `offset_cp_2`. Thus `offset_cp_1 < offset_cp_2`
+    must hold.
 
-    def K(self, X, X2=None):
+    """
+
+    def __init__(self, k1: gpflow.kernels.Kernel, k2: gpflow.kernels.Kernel, offset_cp_1: float, offset_cp_2: float,
+                 variance: float=1.0, variance_cp_1: float=1.0, variance_cp_2: float=1.0):
+        """Initialize changewindow."""
+        if offset_cp_1 >= offset_cp_2:
+            raise RuntimeError(f'Changewindow kernel was initialized with `offset_cp_1 = {offset_cp_1} >= offset_cp_2 = {offset_cp_2}`.')
+
+        super(ChangeWindow, self).__init__([k1, k2])
+
+        self.variance = Param(variance, transform=transforms.positive)
+
+        self.cp_lower = ChangePoint(k1, k2, offset_cp_1, variance_cp_1)
+        self.cp_upper = ChangePoint(self.cp_lower, copy.copy(k1), offset_cp_2, variance_cp_2)
+
+        self.remaining_margin = Param(self.offset_margin, transform=transforms.positive)
+
+    @property
+    def offset_margin(self):
+        return self.cp_upper.offset.value - self.cp_lower.offset.value
+
+    def K(self, X, X2=None):  # noqa: N802, N803
         """Calculate covariance matrix."""
-        if X2 is None:
-            X2 = X
-        XXT = self.variance * tf.squeeze(tf.matmul(tf.expand_dims(X, axis=1), tf.expand_dims(X, axis=2))) + self.offset
-        XXT2 = self.variance * tf.squeeze(tf.matmul(tf.expand_dims(X2, axis=1), tf.expand_dims(X2, axis=2))) + self.offset
-        sigm = tf.sigmoid(XXT)
-        sigm2 = tf.sigmoid(XXT2)
-        sig1 = tf.matmul(tf.expand_dims(sigm, axis=1),
-                         tf.expand_dims(sigm2, axis=0))
-        sig2 = tf.matmul(tf.expand_dims((1. - sigm), axis=1),
-                         tf.expand_dims((1. - sigm2), axis=0))
-        a1 = reduce(tf.multiply,
-                    [sig1, self.kern_list[0].K(X, X2)])
-        a2 = reduce(tf.multiply,
-                    [sig2, self.kern_list[1].K(X, X2)])
-        return reduce(tf.add, [a1, a2])
+        return self.variance * self.cp_upper.K(X, X2)
 
-    def Kdiag(self, X):
+    def Kdiag(self, X):  # noqa: N802, N803
         """Calculate diagonal of covariance matrix only, convenience method."""
-        XXT = self.variance * tf.squeeze(tf.matmul(tf.expand_dims(X, axis=1), tf.expand_dims(X, axis=2))) + self.offset
-        sigm = tf.sigmoid(XXT)
-        a1 = reduce(tf.multiply, [sigm, self.kern_list[0].Kdiag(X),
-                                  sigm])
-        a2 = reduce(tf.multiply, [(1. - sigm), self.kern_list[1].Kdiag(X),
-                                  1. - sigm])
-        return reduce(tf.add, [a1, a2])
+        return self.variance * self.cp_upper.Kdiag(X)
